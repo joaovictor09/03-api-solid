45349a92e0fbb47c89b1ae7fb9b775bdb39d57d7
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-06-10 17:29:14 -0300

    feat: implement user registration functionality with Prisma and bcrypt

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,29 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({
+      email,
+      name,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
