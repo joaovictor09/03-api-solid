37729711d4c52a08027ca2c7166525a0fbeb9687
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-06-12 15:04:49 -0300

    feat: implement refresh token functionality with cookie handling and add related e2e tests

--- src/http/controllers/gyms/nearby.spec.ts
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -45,8 +45,6 @@ describe('Nearby Gyms (e2e)', () => {
       .set('Authorization', `Bearer ${token}`)
       .send()
 
-    console.log(response.body)
-
     expect(response.statusCode).toEqual(200)
     expect(response.body.gyms).toHaveLength(1)
     expect(response.body.gyms).toEqual([
