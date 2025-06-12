851b377e734dda0f991170db716c83c7721dd878
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-06-12 09:35:36 -0300

    feat: integrate JWT authentication with Fastify and add user profile retrieval

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,15 @@
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
-import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    ...user,
+    password_hash: undefined,
+  })
 }
